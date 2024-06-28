91fea401a9c7c2612bc93adfb170efb9e11e229b
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-06-28 20:55:28 +0300

    fix: remove pylint workflow

--- python/Week-01/Day-05_Pair-Of-Socks/week1_day5.py
+++ python/Week-01/Day-05_Pair-Of-Socks/week1_day5.py
@@ -1,17 +1,4 @@
 def sock_pairs(socks: str) -> int:
-    """
-    Count the number of sock pairs.
-
-    This function takes a string representing a collection of socks, where each character
-    represents a sock of a particular color or type. It counts the number of pairs of socks
-    in the collection.
-
-    Parameters:
-    socks (str): A string where each character represents a sock.
-
-    Returns:
-    int: The number of pairs of socks.
-    """
     sock_pairs_count = 0
     for letter in set(socks):
         sock_pairs_count += socks.count(letter) // 2
--- python/Week-01/Day-06_Next-Prime/week1_day6.py
+++ python/Week-01/Day-06_Next-Prime/week1_day6.py
@@ -2,18 +2,6 @@ from sympy import nextprime, isprime
 
 
 def next_prime(number: int) -> int:
-    """
-    Find the next prime number greater than or equal to a given number.
-
-    This function checks if the given number is prime. If it is prime, it returns the number itself.
-    If it is not prime, it finds and returns the smallest prime number greater than the given number.
-
-    Parameters:
-    number (int): The number for which the next prime number is to be found.
-
-    Returns:
-    int: The next prime number greater than or equal to the given number.
-    """
     if isprime(number):
         return number
     return nextprime(number)
