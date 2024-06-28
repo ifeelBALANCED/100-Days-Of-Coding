e355a2d7e113c8e6d558a551fd2c78ea177e6830
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-06-28 18:55:38 +0300

    task: week1 day5

--- python/Week-01/Day-03_Barbecue-Skewers/week1_day3.py
+++ python/Week-01/Day-03_Barbecue-Skewers/week1_day3.py
@@ -1,10 +1,7 @@
 def barbecue_skewers(skewers: list) -> list:
     vegetarian_skewers = 0
-    non_vegetarian_skewers = 0
     for skewer in skewers:
-        if "x" in skewer:
-            non_vegetarian_skewers += 1
-        else:
+        if "x" not in skewer:
             vegetarian_skewers += 1
 
-    return [vegetarian_skewers, non_vegetarian_skewers]
+    return [vegetarian_skewers, len(skewers) - vegetarian_skewers]
