1627b0419469fdf093a58c807948ac337d0bcf19
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-06-28 20:42:51 +0300

    fix: fixed docstrings

--- python/Week-01/Day-04_Is-Johnny-Making-Progress/week1_day4.py
+++ python/Week-01/Day-04_Is-Johnny-Making-Progress/week1_day4.py
@@ -1,4 +1,16 @@
 def johny_progress_days(miles: list) -> int:
+    """
+    Count the number of days Johny made progress.
+
+    This function takes a list of miles Johny ran each day and counts the number of days
+    he ran more miles than the previous day.
+
+    Parameters:
+    miles (list): A list of integers representing the miles Johny ran each day.
+
+    Returns:
+    int: The number of days Johny made progress by running more miles than the previous day.
+    """
     progress_days = 0
     for i in range(len(miles) - 1):
         if miles[i+1] > miles[i]:
