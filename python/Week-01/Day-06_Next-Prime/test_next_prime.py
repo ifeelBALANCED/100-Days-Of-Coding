5bdf75290d6debee9da4486444f08c13e9aa0d8a
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-06-28 20:04:07 +0300

    task: week1 day6(added large numbers to test)

--- python/Week-01/Day-06_Next-Prime/test_next_prime.py
+++ python/Week-01/Day-06_Next-Prime/test_next_prime.py
@@ -12,6 +12,12 @@ from week1_day6 import next_prime
     (-1, 2),
     (17, 17),
     (18, 19),
+    (1000000, 1000003),
+    (999983, 999983),
+    (999984, 1000003),
+    (10000000, 10000019),
+    (100000000, 100000007),
+    (1000000000, 1000000007),
 ])
 def test_next_prime(input_number: int, expected_next_prime_number: int) -> None:
     assert next_prime(input_number) == expected_next_prime_number
