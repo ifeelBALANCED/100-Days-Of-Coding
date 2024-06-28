2287d7b873cfeeb47d5aa77812f73ae3c52ed930
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-06-28 20:42:51 +0300

    fix: fixed docstrings

--- python/Week-01/Day-01_Convert-Ages-To-Days/week1_day1.py
+++ python/Week-01/Day-01_Convert-Ages-To-Days/week1_day1.py
@@ -1,2 +1,11 @@
 def calc_age(age: int) -> int:
+    """
+    Calculate the number of days for a given age in years.
+
+    Parameters:
+    age (int): The age in years.
+
+    Returns:
+    int: The age in days.
+    """
     return age * 365
