bcfa8405b7a16eaa1393c214d4429d74cba0041e
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2024-07-04 00:44:54 +0300

    fixed check_prime task(no cheating), added large numbers to test cases

--- python/Week-01/Day-06_Next-Prime/week1_day6.py
+++ python/Week-01/Day-06_Next-Prime/week1_day6.py
@@ -1,7 +1,24 @@
-from sympy import nextprime, isprime
+def check_if_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 def next_prime(number: int) -> int:
-    if isprime(number):
+    if check_if_prime(number):
         return number
-    return nextprime(number)
+    possible_prime = number
+    while check_if_prime(possible_prime) is False:
+        possible_prime += 1
+
+    return possible_prime
